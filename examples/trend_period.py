ecc3d0b8d70f2f52f51505f613c1bbb8d0228b7d
Alex Kofke <adkofke@buffalo.edu>
2017-06-23 14:18:42 -0400

    Move test data into tests dir, add more tests

--- examples/trend_period.py
+++ examples/trend_period.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from autoperiod import autoperiod
-from autoperiod.helpers import load_google_trends_csv
+from autoperiod.helpers import load_google_trends_csv, load_gpfs_csv
 
 if __name__ == '__main__':
     # values = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3] * 100, np.float)
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     # values = np.array([0, 0, 1, 1] * 10, np.float)
     # times = np.arange(0, values.size, dtype=np.float)
 
-    times, values = load_google_trends_csv(os.path.join(os.getcwd(), "test_data/trends_newyears.csv"))
-    # times, values = load_gpfs_csv("test_data/ub-hpc-6665127-gpfs-reads.csv")
+    # times, values = load_google_trends_csv(os.path.join(os.getcwd(), "tests/data/trends_easter.csv"))
+    times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/ub-hpc-6665127-gpfs-writes.csv"))
 
     # times = np.arange(0, 2, 0.01)
     # values = np.sin(4*np.pi*times)
--- tests/test_autoperiod.py
+++ tests/test_autoperiod.py
@@ -6,7 +6,11 @@ import pytest
 from pytest import approx
 
 from autoperiod import autoperiod
-from autoperiod.helpers import load_google_trends_csv
+from autoperiod.helpers import load_google_trends_csv, load_gpfs_csv
+
+
+def data(name):
+    return os.path.join(os.path.dirname(__file__), "data", name)
 
 def test_clean_sinwave():
     times = np.arange(0, 10, 0.01)
@@ -41,7 +45,25 @@ def test_squarewave():
 
 
 def test_trends_newyears():
-    times, values = load_google_trends_csv(os.getcwd() + "/../test_data/trends_newyears.csv")
+    times, values = load_google_trends_csv(data("trends_newyears.csv"))
     period = autoperiod(times, values)
     # within 3% of "expected" period
     assert period == approx(365, rel=0.03)
+
+
+def test_trends_easter():
+    times, values = load_google_trends_csv(data("trends_easter.csv"))
+    period = autoperiod(times, values)
+    # Easter isn't a fixed holiday, so the "expected" period won't be as close to 365 days
+    assert period == approx(365, rel=0.05)
+
+
+def test_gpfs_reads():
+    times, values = load_gpfs_csv(data("ub-hpc-6665127-gpfs-reads.csv"))
+    period = autoperiod(times, values)
+    assert period == approx(9469, rel=0.0001)
+
+def test_gpfs_writes():
+    times, values = load_gpfs_csv(data("ub-hpc-6665127-gpfs-writes.csv"))
+    period = autoperiod(times, values)
+    assert period == approx(9560, abs=5)
