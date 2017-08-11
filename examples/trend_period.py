0fdd8336e597767c791ed2911a60d415e9c8e864
Alex Kofke <adkofke@buffalo.edu>
2017-08-11 10:26:36 -0400

    Another example data, remove 32 backports from requirements.txt

--- examples/trend_period.py
+++ examples/trend_period.py
@@ -13,43 +13,43 @@ if __name__ == '__main__':
     # values = 1 - values
     # times = np.arange(1, values.size + 1, dtype=np.float)
 
-    # times, values = load_google_trends_csv(os.path.join(os.getcwd(), "tests/data/trends_easter.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_google_trends_csv(os.path.join(os.getcwd(), "tests/data/trends_python.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/ub-hpc-6665127-gpfs-writes.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_google_trends_csv(os.path.join(os.getcwd(), "tests/data/trends_newyears.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_google_trends_csv(os.path.join(os.getcwd(), "tests/data/trends_summer.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/industry-2895978-gpfs-reads.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/industry-2901023-gpfs-writes.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/industry-2896041-gpfs-writes.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/chemistry-1455991-gpfs-writes.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/chemistry-1455991-gpfs-writes-cpn-f15-08.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
-    # times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/ub-hpc-writes-cpn-k16-25-01.csv"))
-    # p = Autoperiod(times, values, plotter=Plotter())
-    #
+    times, values = load_google_trends_csv(os.path.join(os.getcwd(), "tests/data/trends_easter.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_google_trends_csv(os.path.join(os.getcwd(), "tests/data/trends_python.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/ub-hpc-6665127-gpfs-writes.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_google_trends_csv(os.path.join(os.getcwd(), "tests/data/trends_newyears.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_google_trends_csv(os.path.join(os.getcwd(), "tests/data/trends_summer.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/industry-2895978-gpfs-reads.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/industry-2901023-gpfs-writes.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/industry-2896041-gpfs-writes.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/chemistry-1455991-gpfs-writes.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/chemistry-1455991-gpfs-writes-cpn-f15-08.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
+    times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/ub-hpc-writes-cpn-k16-25-01.csv"))
+    p = Autoperiod(times, values, plotter=Plotter())
+
     # values = np.array([0, 0, 1, 1] * 10, np.float)
     # times = np.arange(0, values.size, dtype=np.float)
     # period = autoperiod(times, values, plot=True, delay_show=True, verbose_plot=False, threshold_method='mc')
-
+    #
     # times, values = load_gpfs_csv(os.path.join(os.getcwd(), "tests/data/industry-writes-2904963-cpn-m28-15-01.csv"))
     # p = autoperiod(times, values, plot=True, verbose_plot=False, delay_show=True)
 
